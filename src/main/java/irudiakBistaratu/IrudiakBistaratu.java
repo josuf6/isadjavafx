e6aa8c850ccfeb11b16caed353b87d94d4a43043
josuf6 <josuf6@gmail.com>
2020-10-03 13:08:54 +0200

    IrudiakBistaratu klasea sortu, irudiakBistaratu paketean main() eta staart() metodoekin

---
+++ src/main/java/irudiakBistaratu/IrudiakBistaratu.java
@@ -0,0 +1,15 @@
+package irudiakBistaratu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class IrudiakBistaratu extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
