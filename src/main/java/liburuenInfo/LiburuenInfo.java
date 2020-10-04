8e205761644dce0b4a8ccdfab91a9ec04c9954c6
josuf6 <josuf6@gmail.com>
2020-10-04 17:42:56 +0200

    LiburuenInfo klasea sortu

---
+++ src/main/java/liburuenInfo/LiburuenInfo.java
@@ -0,0 +1,16 @@
+package liburuenInfo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LiburuenInfo extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
